8ba7f5ca87f3c46e592281935afd88e21a740b59
mua-th <mouadelgharbi2002@gmail.com>
2024-11-13 17:28:19 +0100

    Revert "screen full"
    This reverts commit ca7bdb4ef391ff1912730e0b3fddaead8245ccd8.

--- frontiaa/src/app/layout.tsx
+++ frontiaa/src/app/layout.tsx
@@ -19,9 +19,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body>
-        {children}
-        </body>
+      <body>{children}</body>
     </html>
   );
 }
