ca7bdb4ef391ff1912730e0b3fddaead8245ccd8
grainme <marouanebouf3@gmail.com>
2024-11-13 17:09:44 +0100

    screen full

--- frontiaa/src/app/layout.tsx
+++ frontiaa/src/app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        </body>
     </html>
   );
 }
