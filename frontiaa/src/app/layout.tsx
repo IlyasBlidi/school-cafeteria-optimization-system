110495a8e8535eeade2bb99af19dbf33b44cbdc3
grainme <marouanebouf3@gmail.com>
2025-01-20 22:39:20 +0100

    [solved]

--- frontiaa/src/app/layout.tsx
+++ frontiaa/src/app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
       </head>
       <body>
         <NotificationProvider>
-          <NotificationComponent />
           {children}
           <Toaster />
         </NotificationProvider>
